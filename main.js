fc352fecc587b8dfd7e6078c2c07b0f084bab039
Cristi Gutierrez <65434648+justbycris@users.noreply.github.com>
2020-09-01 22:49:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,44 +5,44 @@ function zodiac() {
     var error = "Error. Please enter a valid date.";
 
 
-    if (day >= 32 || day === 0 || month === 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december') {
-        return document.getElementById("yoursign").innerHTML = error;
 
-    } else if (month === 'january' && day >= 20 || month === 'february' && day <= 18) {
-        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is Aquarius!`);
+    if (month === 'january' && day >= 20 || month === 'february' && day <= 18) {
+        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is Aquarius.`);
 
     } else if (month === 'february' && day >= 19 || month === 'march' && day <= 20) {
-        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is Pisces!`);
+        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is Pisces.`);
 
     } else if (month === 'march' && day >= 21 || month === 'april' && day <= 19) {
-        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is an Aries!`);
+        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is an Aries.`);
 
     } else if (month === 'april' && day >= 20 || month === 'may' && day <= 20) {
-        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is Taurus!`);
+        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is Taurus.`);
 
     } else if (month === 'may' && day >= 21 || month === 'june' && day <= 20) {
-        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is Gemini!`);
+        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is Gemini.`);
 
     } else if (month === 'june' && day >= 21 || month === 'july' && day <= 22) {
-        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is Cancer!`);
+        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is Cancer.`);
 
     } else if (month === 'july' && day >= 23 || month === 'august' && day <= 22) {
-        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is Leo!`);
+        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is Leo.`);
 
     } else if (month === 'august' && day >= 23 || month === 'september' && day <= 23) {
-        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is Virgo!`);
+        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is Virgo.`);
 
     } else if (month === 'september' && day >= 24 || month === 'october' && day <= 22) {
-        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is Libra!`);
+        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is Libra.`);
 
     } else if (month === 'october' && day >= 23 || month === 'november' && day <= 21) {
-        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is Scorpio!`);
+        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is Scorpio.`);
 
     } else if (month === 'november' && day >= 22 || month === 'december' && day <= 19) {
-        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is Sagittarius`);
+        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is Sagittarius.`);
 
-    } else(month === 'december' && day >= 20 || month === 'january' && day <= 19); {
-        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is Capricorn!`);
+    } else if (month === 'december' && day >= 20 || month === 'january' && day <= 19) {
+        return (document.getElementById('yoursign').innerHTML = `Hey ${name}, your astrology sign is Capricorn.`);
 
+    } else(day >= 32 || day === 0); {
+        return document.getElementById("yoursign").innerHTML = error;
     }
 }
